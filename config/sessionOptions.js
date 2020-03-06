d62ff7655c7c70c6af827a8ddca22a1481d4b533
Jeremy <me@jeremypoole.ca>
2020-03-05 17:39:36 -0800

    temporarily disable secure cookies to troubleshoot broken prod, no cookies set

--- config/sessionOptions.js
+++ config/sessionOptions.js
@@ -3,7 +3,7 @@
  */
 const session = require('express-session')
 const MemcachedStore = require('connect-memcached')(session)
-const isProd = require('./isProd')
+// const isProd = require('./isProd')
 
 const sessionOptions = {
   key: 'tsid',
@@ -15,7 +15,8 @@ const sessionOptions = {
     maxAge: 14*24*60*60*1000,
     credentials: true,
     sameSite: 'strict',
-    secure: isProd
+    // commented out to try and figure out why cookies broke in prod
+    // secure: isProd
   },
   store: new MemcachedStore({
     // default port for memcached. it is the same in prod and dev.
